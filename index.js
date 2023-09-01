ef208720a9e907e0b59a4a128884c123d5ebcc2c
Michelle <michgabz54@gmail.com>
2023-09-01 16:25:34 +0800

    Add edit

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-function Chat () {
-     alert("Good Morning Philippines!");
+function chat() {
+   alert("Good morning Philippines");
 
-     const myName = prompt("What is your name?");
+   const myName = prompt("What is your name?");
+   alert("Hello,"+ myName + "!");
 
-     alert("Hello,"+ myName + "!");
-
-     const subject = prompt("Do you like our place?");
-
-     if (subject == "Yes"){
-
-        alert("This place is amazing!");
-     }
-
-      else if(subject == "No") {
-
-        alert("New place");
-    }
-       else {
-        alert("Explore");
-       }
+   const subject = prompt("Do you like our place?");
+   if (subject == "Yes") {
+      alert("This place is amazing!");
+   }
+   else if (subject == "No"){
+      alert("Curious");
+   }
+   else {
+      alert("Explore");
+   }
 }
